d3a955f81396186b3ca4ac16f7f3c9b2f95c5f37
vsvoice <erikblomqvist1@gmail.com>
2026-03-27 11:50:40 +0200

    Added ip

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return { "msg": "Tjenixen igen!", "v": "0.2" }
+
+@app.get("/api/ip")
+def read_root(request: Request):
+    client_host = request.client.host
+    return {"client_host": client_host}
 
 
 @app.get("/items/{id}")
